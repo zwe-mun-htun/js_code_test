7feb7b57f1d4909d74c6357dcd5b51227e64bb58
zwe-mun-htun <zwemunhtun@teclinkmyanmar.com>
2024-05-17 18:00:14 +0700

    refactor(folder-structure): added clean architecture folder structure, values configs files, responsive widget

---
+++ lib/src/presentations/values/sizes.dart
@@ -0,0 +1,201 @@
+part of 'values.dart';
+
+class Sizes {
+  static const double size120 = 120.0;
+  static const double size60 = 60.0;
+  static const double size48 = 48.0;
+  static const double size45 = 45.0;
+  static const double size40 = 40.0;
+  static const double size36 = 36.0;
+  static const double size30 = 30.0;
+  static const double size24 = 24.0;
+  static const double size22 = 22.0;
+  static const double size20 = 20.0;
+  static const double size18 = 18.0;
+  static const double size16 = 16.0;
+  static const double size15 = 15.0;
+  static const double size12 = 12.0;
+  static const double size10 = 10.0;
+  static const double size8 = 8.0;
+  static const double size6 = 6.0;
+  static const double size5 = 5.0;
+  static const double size4 = 4.0;
+  static const double size2 = 2.0;
+  static const double size1 = 1.0;
+  static const double size0 = 0.0;
+
+  //TextSizes
+  static const double textSize96 = 96.0;
+  static const double textSize60 = 60.0;
+  static const double textSize50 = 50.0;
+  static const double textSize48 = 48.0;
+  static const double textSize44 = 44.0;
+  static const double textSize40 = 40.0;
+  static const double textSize36 = 36.0;
+  static const double textSize34 = 34.0;
+  static const double textSize32 = 32.0;
+  static const double textSize30 = 30.0;
+  static const double textSize28 = 28.0;
+  static const double textSize24 = 24.0;
+  static const double textSize22 = 22.0;
+  static const double textSize20 = 20.0;
+  static const double textSize18 = 18.0;
+  static const double textSize16 = 16.0;
+  static const double textSize14 = 14.0;
+  static const double textSize12 = 12.0;
+  static const double textSize10 = 10.0;
+  static const double textSize8 = 8.0;
+
+  //IconSizes
+  static const double iconSize50 = 50.0;
+  static const double iconSize40 = 40.0;
+  static const double iconSize32 = 32.0;
+  static const double iconSize30 = 30.0;
+  static const double iconSize24 = 24.0;
+  static const double iconSize22 = 22.0;
+  static const double iconSize20 = 20.0;
+  static const double iconSize18 = 18.0;
+  static const double iconSize16 = 16.0;
+  static const double iconSize14 = 14.0;
+  static const double iconSize12 = 12.0;
+  static const double iconSize10 = 10.0;
+  static const double iconSize8 = 8.0;
+
+  //Heights
+  static const double height300 = 300.0;
+  static const double height240 = 240.0;
+  static const double height200 = 200.0;
+  static const double height180 = 180.0;
+  static const double height160 = 160.0;
+  static const double height150 = 150.0;
+  static const double height130 = 130.0;
+  static const double height100 = 100.0;
+  static const double height60 = 60.0;
+  static const double height50 = 50.0;
+  static const double height48 = 48.0;
+  static const double height46 = 46.0;
+  static const double height44 = 44.0;
+  static const double height40 = 40.0;
+  static const double height36 = 36.0;
+  static const double height35 = 35.0;
+  static const double height32 = 32.0;
+  static const double height30 = 30.0;
+  static const double height25 = 25.0;
+  static const double height24 = 24.0;
+  static const double height22 = 22.0;
+  static const double height20 = 20.0;
+  static const double height18 = 18.0;
+  static const double height16 = 16.0;
+  static const double height14 = 14.0;
+  static const double height10 = 10.0;
+  static const double height8 = 8.0;
+  static const double height4 = 4.0;
+  static const double height3 = 3.0;
+  static const double height2 = 2.0;
+  static const double height1 = 1.0;
+
+  //Widths
+  static const double width300 = 300.0;
+  static const double width236 = 236.0;
+  static const double width270 = 270.0;
+  static const double width200 = 200.0;
+  static const double width180 = 180.0;
+  static const double width170 = 170.0;
+  static const double width160 = 160.0;
+  static const double width150 = 150.0;
+  static const double width120 = 120.0;
+  static const double width100 = 100.0;
+  static const double width80 = 80.0;
+  static const double width60 = 60.0;
+  static const double width50 = 50.0;
+  static const double width40 = 40.0;
+  static const double width32 = 32.0;
+  static const double width30 = 30.0;
+  static const double width25 = 25.0;
+  static const double width24 = 24.0;
+  static const double width22 = 22.0;
+  static const double width20 = 20.0;
+  static const double width18 = 18.0;
+  static const double width16 = 16.0;
+  static const double width14 = 14.0;
+  static const double width12 = 12.0;
+  static const double width10 = 10.0;
+  static const double width8 = 8.0;
+  static const double width6 = 6.0;
+  static const double width4 = 4.0;
+  static const double width2 = 2.0;
+  static const double width1 = 1.0;
+  static const double width0 = 0.0;
+
+  //Margins
+  static const double margin200 = 200.0;
+  static const double margin60 = 60.0;
+  static const double margin48 = 48.0;
+  static const double margin46 = 46.0;
+  static const double margin44 = 44.0;
+  static const double margin40 = 40.0;
+  static const double margin36 = 36.0;
+  static const double margin32 = 32.0;
+  static const double margin30 = 30.0;
+  static const double margin26 = 26.0;
+  static const double margin24 = 24.0;
+  static const double margin22 = 22.0;
+  static const double margin20 = 20.0;
+  static const double margin18 = 18.0;
+  static const double margin16 = 16.0;
+  static const double margin14 = 14.0;
+  static const double margin12 = 12.0;
+  static const double margin10 = 10.0;
+  static const double margin8 = 8.0;
+  static const double margin4 = 4.0;
+  static const double margin0 = 0.0;
+
+  //Paddings
+  static const double padding40 = 40.0;
+  static const double padding36 = 36.0;
+  static const double padding32 = 32.0;
+  static const double padding24 = 24.0;
+  static const double padding22 = 22.0;
+  static const double padding20 = 20.0;
+  static const double padding18 = 18.0;
+  static const double padding16 = 16.0;
+  static const double padding14 = 14.0;
+  static const double padding12 = 12.0;
+  static const double padding10 = 10.0;
+  static const double padding8 = 8.0;
+  static const double padding4 = 4.0;
+  static const double padding2 = 2.0;
+  static const double padding0 = 0.0;
+
+  //Radius
+  static const double radius80 = 80.0;
+  static const double radius70 = 70.0;
+  static const double radius60 = 60.0;
+  static const double radius40 = 40.0;
+  static const double radius32 = 32.0;
+  static const double radius30 = 30.0;
+  static const double radius24 = 24.0;
+  static const double radius22 = 22.0;
+  static const double radius20 = 20.0;
+  static const double radius18 = 18.0;
+  static const double radius16 = 16.0;
+  static const double radius14 = 14.0;
+  static const double radius12 = 12.0;
+  static const double radius10 = 10.0;
+  static const double radius8 = 8.0;
+  static const double radius6 = 6.0;
+  static const double radius4 = 4.0;
+  static const double radius0 = 0.0;
+
+  //Elevations
+  static const double elevation16 = 16.0;
+  static const double elevation14 = 14.0;
+  static const double elevation12 = 12.0;
+  static const double elevation10 = 10.0;
+  static const double elevation8 = 8.0;
+  static const double elevation6 = 6.0;
+  static const double elevation4 = 4.0;
+  static const double elevation2 = 2.0;
+  static const double elevation1 = 1.0;
+  static const double elevation0 = 0.0;
+}
---
+++ lib/src/presentations/values/strings.dart
@@ -0,0 +1,9 @@
+part of 'values.dart';
+
+class StringConst {
+  static String kMaterialAppTitle = "JS Code Test";
+
+  static String kCreateDetectionTitle = "Create Detection";
+
+  static String kDetectionsListTitle = "Detections List";
+}
